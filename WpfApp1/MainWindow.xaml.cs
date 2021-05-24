a13f2b86255772d2ca387899d787b6893358596d
Devin Morrow <DMorrow@alphatecspine.com>
2021-05-24 14:55:06 -0700

    upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double _testOnset = 100;
+        private double _testPeak = 200;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            TestOnset = 100;
+            TestPeak = 200;
+        }
+
+        public double TestOnset
+        {
+            get { return _testOnset; }
+            set { _testOnset = value; }
+        }
+
+        public double TestPeak
+        {
+            get { return _testPeak; }
+            set { _testPeak = value; }
+        }
+    }
+}
